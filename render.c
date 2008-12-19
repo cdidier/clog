dbfbdfcc5786e039971f4b7243c4d0d687eac52a
Colin Didier <cdidier@cybione.org>
2008-12-19 08:10:57 +0000

    introduce parse_markers() to factorize some code

--- render.c
+++ render.c
@@ -35,6 +35,21 @@ static char rcsid[] = "$Id$";
 
 #include "common.h"
 
+typedef void (parse_markers_cb)(const char *, void *);
+
+struct data_ac {
+	const char	*aname;
+	const char	*title;
+	const struct tm	*tm;
+	FILE		*fbody;
+	uint		*nb_comments;
+};
+
+struct data_p {
+	page_cb		*cb;
+	const char	*name;
+};
+
 int	read_article(const char *, article_cb, char *, size_t);
 void	read_articles(article_cb);
 ulong	read_article_tags(const char *, article_tag_cb);
@@ -47,6 +62,25 @@ extern gzFile	 gz;
 
 #ifdef ENABLE_COMMENTS
 ulong	read_comments(const char *, comment_cb);
+
+struct data_c {
+	const char	*author;
+	const struct tm	*tm;
+	const char	*ip;
+	const char	*mail;
+	const char	*web;
+	FILE		*fbody;
+};
+
+#ifdef ENABLE_POST_COMMENT
+struct data_cf {
+	const char	*aname;
+	char		*hash;
+	int		*jam1;
+	int		*jam2;
+};
+#endif /* ENABLE_POST_COMMENT */
+
 #endif /* ENABLE_COMMENTS */
 
 #ifdef ENABLE_STATIC
@@ -56,10 +90,28 @@ void	add_static_article(const char *);
 extern int from_cmd, generating_static;
 #endif /* ENABLE_STATIC */
 
-#define TAG "%%"
+#define MARKER_TAG "%%"
 
 static uint	 nb_articles;
 extern FILE	*hout;
+
+static FILE *
+open_template(const char *file)
+{
+	FILE *fin;
+	char path[MAXPATHLEN];
+
+#ifdef ENABLE_STATIC
+	if (from_cmd)
+		snprintf(path, MAXPATHLEN, CHROOT_DIR TEMPLATES_DIR "/%s",
+		   file);
+	else
+#endif /* ENABLE_STATIC */
+		snprintf(path, MAXPATHLEN, TEMPLATES_DIR "/%s", file);
+	if ((fin = fopen(path, "r")) == NULL)
+		 warn("fopen: %s", path);
+	return fin;
+}
 
 static void
 hputc(const char c)
@@ -231,148 +283,175 @@ hput_pagelink(const char *tname, long page, const char *text)
 #endif /* ENABLE_STATIC */
 }
 
-static FILE *
-open_template(const char *file)
-{
-	FILE *fin;
-	char path[MAXPATHLEN];
-
-#ifdef ENABLE_STATIC
-	if (from_cmd)
-		snprintf(path, MAXPATHLEN, CHROOT_DIR TEMPLATES_DIR "/%s",
-		   file);
-	else
-#endif /* ENABLE_STATIC */
-		snprintf(path, MAXPATHLEN, TEMPLATES_DIR "/%s", file);
-	if ((fin = fopen(path, "r")) == NULL)
-		 warn("fopen: %s", path);
-	return fin;
-}
-
-void
-render_error(const char *msg)
-{
-	FILE *fin;
-	char buf[BUFSIZ], *a, *b;
-
-	if ((fin = open_template("error.html")) == NULL)
-		return;
-	while (fgets(buf, BUFSIZ, fin) != NULL) {
-		buf[strcspn(buf, "\n")] = '\0';
-		for (a = buf; (b = strstr(a, TAG)) != NULL; a = b+2) {
-			*b = '\0';
-			hputs(a);
-			a = b+2;
-			if ((b = strstr(a, TAG)) != NULL) {
-				*b = '\0';
-				if (strcmp(a, "MESSAGE") == 0)
-					hputs(msg);
-			}
-		}
-		hputs(a);
-		hputc('\n');
-	}
-	fclose(fin);
-}
-
-static void
-render_article_tag(const char *tname)
-{
-	hput_pagelink(tname, 0, tname);
-	hputc(' ');
-}
-
 static int
-render_generic_markers(const char *a)
+hput_generic_markers(const char *m)
 { 
 	extern const char *tag;
 
-	if (strcmp(a, "BASE_URL") == 0)
+	if (strcmp(m, "BASE_URL") == 0)
 		hput_url(NULL, NULL);
-	else if (strcmp(a, "SITE_NAME") == 0)
+	else if (strcmp(m, "SITE_NAME") == 0)
 		hputs(SITE_NAME);
-	else if (strcmp(a, "DESCRIPTION") == 0)
+	else if (strcmp(m, "DESCRIPTION") == 0)
 		hputs(DESCRIPTION);
-	else if (strcmp(a, "COPYRIGHT") == 0)
+	else if (strcmp(m, "COPYRIGHT") == 0)
 		hputs(COPYRIGHT);
-	else if (strcmp(a, "CHARSET") == 0)
+	else if (strcmp(m, "CHARSET") == 0)
 		hputs(CHARSET);
-	else if (strcmp(a, "RSS_TITLE") == 0) {
+	else if (strcmp(m, "RSS_TITLE") == 0) {
 		hputs(SITE_NAME);
 		hputs(" RSS");
 		if (tag != NULL) {
 			hputs(" - tag:");
 			hputs(tag);
 		}
-	} else if (strcmp(a, "RSS_URL") == 0)
+	} else if (strcmp(m, "RSS_URL") == 0)
 		hput_url("rss", tag);
 	else
 		return 0;
 	return 1;
 }
 
+static void
+parse_markers(FILE *fin, parse_markers_cb cb, void *data)
+{
+	char buf[BUFSIZ], *a, *b;
+
+	while (fgets(buf, BUFSIZ, fin) != NULL) {
+		buf[strcspn(buf, "\n")] = '\0';
+		for (a = buf; (b = strstr(a, MARKER_TAG)) != NULL; a = b+2) {
+			*b = '\0';
+			hputs(a);
+			a = b+2;
+			if ((b = strstr(a, MARKER_TAG)) != NULL) {
+				*b = '\0';
+				if (hput_generic_markers(a));
+				else
+					cb(a, data);
+			}
+		}
+		hputs(a);
+		hputc('\n');
+	}
+}
+
+static void
+markers_error(const char *m, void *data)
+{
+	const char *msg = data;
+
+	if (strcmp(m, "MESSAGE") == 0)
+		hputs(msg);
+}
+
+void
+render_error(const char *msg)
+{
+	FILE *fin;
+
+	if ((fin = open_template("error.html")) == NULL)
+		return;
+	parse_markers(fin, markers_error, (void *)msg);
+	fclose(fin);
+}
+
 #ifdef ENABLE_COMMENTS
+
+static void
+markers_comment(const char *m, void *data)
+{
+	struct data_c *d = data;
+	char date[TIME_LENGTH+1], body[BUFSIZ];
+
+	if (strcmp(m, "NAME") == 0) {
+		hput_escaped(d->author);
+	} else if (strcmp(m, "DATE") == 0) {
+		strftime(date, TIME_LENGTH+1, TIME_FORMAT, d->tm);
+		hputs(date);
+	} else if (strcmp(m, "IP") == 0) {
+		hputs(d->ip);
+	} else if (strcmp(m, "MAIL") == 0) {
+		if (*d->mail != '\0') {
+			hputs("<a href=\"mailto:");
+			hput_escaped(d->mail);
+			hputs("\">mail</a>");
+		}
+	} else if (strcmp(m, "WEB") == 0) {
+		if (*d->web != '\0') {
+			hputs("<a href=\"");
+			hput_escaped(d->web);
+			hputs("\">web</a>");
+		}
+	} else if (strcmp(m, "TEXT") == 0) {
+		while (fgets(body, BUFSIZ, d->fbody) != NULL)
+			hput_escaped(body);
+	}
+}
 
 static void
 render_comment(const char *author, const struct tm *tm, const char *ip,
     const char *mail, const char *web, FILE *fbody)
 {
 	FILE *fin;
-	char buf[BUFSIZ], *a, *b;
-	char date[TIME_LENGTH+1], body[BUFSIZ];
+	struct data_c d = { author, tm, ip, mail, web, fbody };
 
 	if ((fin = open_template("comment.html")) == NULL)
 		return;
-	while (fgets(buf, BUFSIZ, fin) != NULL) {
-		buf[strcspn(buf, "\n")] = '\0';
-		for (a = buf; (b = strstr(a, TAG)) != NULL; a = b+2) {
-			*b = '\0';
-			hputs(a);
-			a = b+2;
-			if ((b = strstr(a, TAG)) != NULL) {
-				*b = '\0';
-				if (render_generic_markers(a));
-				else if (strcmp(a, "NAME") == 0)
-					hput_escaped(author);
-				else if (strcmp(a, "DATE") == 0) {
-					strftime(date, TIME_LENGTH+1,
-					    TIME_FORMAT, tm);
-					hputs(date);
-				} else if (strcmp(a, "IP") == 0)
-					hputs(ip);
-				else if (*mail != '\0'
-				    && strcmp(a, "MAIL") == 0) {
-					hputs("<a href=\"mailto:");
-					hput_escaped(mail);
-					hputs("\">mail</a>");
-				} else if (*web != '\0'
-				    && strcmp(a, "WEB") == 0) {
-					hputs("<a href=\"");
-					hput_escaped(web);
-					hputs("\">web</a>");
-				} else if (strcmp(a, "TEXT") == 0) {
-					while (fgets(body, BUFSIZ,
-					    fbody) != NULL)
-						hput_escaped(body);
-				}
-			}
-		}
-		hputs(a);
-		hputc('\n');
-	}
+	parse_markers(fin, markers_comment, &d);
 	fclose(fin);
 }
 
 #ifdef ENABLE_POST_COMMENT
 
+#define FORM_PREFIX	"FORM_"
+#define FORM_LEN	sizeof("FORM_")-1
+
+static void
+markers_comment_form(const char *m, void *data)
+{
+	struct data_cf *d = data;
+	extern struct cform comment_form;
+
+	if (strcmp(m, "POST_URL") == 0) {
+		hputs(BIN_URL"/");
+		hputs(d->aname);
+	} else if (strcmp(m, "JAM_HASH") == 0) {
+		hputs(d->hash);
+	} else if (strcmp(m, "JAM1") == 0) {
+		hputs("&#");
+		hputd(*d->jam1+48);
+		hputc(';');
+	} else if (strcmp(m, "JAM2") == 0) {
+		hputs("&#");
+		hputd(*d->jam2+48);
+		hputc(';');
+	} else if (strncmp(m, FORM_PREFIX, FORM_LEN) == 0) {
+		if (strcmp(m+FORM_LEN, "NAME") == 0) {
+			if (comment_form.name != NULL)
+				hput_escaped(comment_form.name);
+		} else if (strcmp(m+FORM_LEN, "MAIL") == 0) {
+			if (comment_form.mail != NULL)
+				hput_escaped(comment_form.mail);
+		} else if (strcmp(m+FORM_LEN, "WEB") == 0) {
+			if (comment_form.web != NULL)
+				hput_escaped(comment_form.web);
+		} else if (strcmp(m+FORM_LEN, "TEXT") == 0) {
+			if (comment_form.text != NULL)
+				hput_escaped(comment_form.text);
+		} else if (strcmp(m+FORM_LEN, "ERROR") == 0) {
+			if (comment_form.error != NULL)
+				hputs(comment_form.error);
+		}
+	}
+}
+
 static void
 render_comment_form(const char *aname)
 {
 	FILE *fin;
-	char buf[BUFSIZ], *a, *b;
 	int jam1, jam2;
 	char salt[sizeof(JAM_SALT)+1], hash[SHA1_DIGEST_STRING_LENGTH];
-	extern struct cform comment_form;
+	struct data_cf d = { aname, hash, &jam1, &jam2 };
 
 	if ((fin = open_template("comment_form.html")) == NULL)
 		return;
@@ -382,142 +461,91 @@ render_comment_form(const char *aname)
 	strlcpy(salt+1, JAM_SALT, sizeof(JAM_SALT));
 	*salt = jam1+jam2;
 	SHA1Data(salt, sizeof(JAM_SALT), hash);
-	while (fgets(buf, BUFSIZ, fin) != NULL) {
-		buf[strcspn(buf, "\n")] = '\0';
-		for (a = buf; (b = strstr(a, TAG)) != NULL; a = b+2) {
-			*b = '\0';
-			hputs(a);
-			a = b+2;
-			if ((b = strstr(a, TAG)) != NULL) {
-				*b = '\0';
-				if (render_generic_markers(a));
-				else if (strcmp(a, "POST_URL") == 0) {
-					hputs(BIN_URL"/");
-					hputs(aname);
-				} else if (strcmp(a, "JAM_HASH") == 0)
-					hputs(hash);
-				else if (strcmp(a, "JAM1") == 0) {
-					hputs("&#");
-					hputd(jam1+48);
-					hputc(';');
-				} else if (strcmp(a, "JAM2") == 0) {
-					hputs("&#");
-					hputd(jam2+48);
-					hputc(';');
-				} else if (comment_form.name != NULL
-				    && strcmp(a, "FORM_NAME") == 0)
-					hput_escaped(comment_form.name);
-				else if (comment_form.mail
-				    && strcmp(a, "FORM_MAIL") == 0)
-					hput_escaped(comment_form.mail);
-				else if (comment_form.web != NULL
-				    && strcmp(a, "FORM_WEB") == 0)
-					hput_escaped(comment_form.web);
-				else if (comment_form.text != NULL
-				    && strcmp(a, "FORM_TEXT") == 0)
-					hput_escaped(comment_form.text);
-				else if (comment_form.error != NULL
-				    && strcmp(a, "FORM_ERROR") == 0)
-					hputs(comment_form.error);
-			}
-		}
-		hputs(a);
-		hputc('\n');
-	}
+	parse_markers(fin, markers_comment_form, &d);
 	fclose(fin);
 }
 
 #endif /* ENABLE_POST_COMMENT */
 
 static void
+markers_comments(const char *m, void *data)
+{
+	const char *aname = data;
+
+	if (strcmp(m, "COMMENTS") == 0)
+		read_comments(aname, render_comment);
+#ifdef ENABLE_POST_COMMENT
+	else if (strcmp(m, "COMMENT_FORM") == 0)
+		render_comment_form(aname);
+#endif /* ENABLE_POST_COMMENT */
+}
+
+static void
 render_comments(const char *aname)
 {
 	FILE *fin;
-	char buf[BUFSIZ], *a, *b;
 
 	if ((fin = open_template("comments.html")) == NULL)
 		return;
-	while (fgets(buf, BUFSIZ, fin) != NULL) {
-		buf[strcspn(buf, "\n")] = '\0';
-		for (a = buf; (b = strstr(a, TAG)) != NULL; a = b+2) {
-			*b = '\0';
-			hputs(a);
-			a = b+2;
-			if ((b = strstr(a, TAG)) != NULL) {
-				*b = '\0';
-				if (render_generic_markers(a));
-				else if (strcmp(a, "COMMENTS") == 0)
-					read_comments(aname,
-					    render_comment);
-#ifdef ENABLE_POST_COMMENT
-				else if (strcmp(a, "COMMENT_FORM") == 0)
-					render_comment_form(aname);
-#endif /* ENABLE_POST_COMMENT */
-			}
-		}
-		hputs(a);
-		hputc('\n');
-	}
+	parse_markers(fin, markers_comments, (void *)aname);
 	fclose(fin);
 }
 
 #endif /* ENABLE_COMMENTS */
+
+static void
+render_article_tag(const char *tname)
+{
+	hput_pagelink(tname, 0, tname);
+	hputc(' ');
+}
+
+static void
+markers_article_content(const char *m, void *data)
+{
+	struct data_ac *d = data;
+	char date[TIME_LENGTH+1], body[BUFSIZ];
+
+	if (strcmp(m, "TITLE") == 0) {
+		hputs(d->title);
+	} else if (strcmp(m, "DATE") == 0) {
+		strftime(date, TIME_LENGTH+1, TIME_FORMAT, d->tm);
+		hputs(date);
+	} else if (strcmp(m, "TAGS") == 0) {
+		if (read_article_tags(d->aname, render_article_tag) == 0)
+			hputs(NO_TAG);
+	} else if (strcmp(m, "BODY") == 0) {
+		while (fgets(body, BUFSIZ, d->fbody) != NULL)
+			hputs(body);
+	} else if (strcmp(m, "URL") == 0) {
+		hput_url(d->aname, NULL);
+#ifdef ENABLE_COMMENTS
+	} else if (strcmp(m, "NB_COMMENTS") == 0) {
+		switch (*d->nb_comments) {
+		case 0:
+			hputs("no comment");
+			break;
+		case 1:
+			hputs("1 comment");
+			break;
+		default:
+			hputd(*d->nb_comments);
+			hputs(" comments");
+		}
+	}
+#endif /* ENABLE_COMMENTS */
+}
 
 static void
 render_article_content(const char *aname, const char *title,
     const struct tm *tm, FILE *fbody, uint nb_comments)
 {
 	FILE *fin;
-	char buf[BUFSIZ], *a, *b;
-	char date[TIME_LENGTH+1], body[BUFSIZ];
+	struct data_ac d = { aname, title, tm, fbody, &nb_comments };
 
 	if ((fin = open_template("article.html")) == NULL)
 		return;
-	while (fgets(buf, BUFSIZ, fin) != NULL) {
-		buf[strcspn(buf, "\n")] = '\0';
-		for (a = buf; (b = strstr(a, TAG)) != NULL; a = b+2) {
-			*b = '\0';
-			hputs(a);
-			a = b+2;
-			if ((b = strstr(a, TAG)) != NULL) {
-				*b = '\0';
-				if (render_generic_markers(a));
-				else if (strcmp(a, "TITLE") == 0) {
-					hputs(title);	
-				} else if (strcmp(a, "DATE") == 0) {
-					strftime(date, TIME_LENGTH+1,
-					    TIME_FORMAT, tm);
-					hputs(date);
-				} else if (strcmp(a, "TAGS") == 0) {
-					if (read_article_tags(aname,
-					    render_article_tag) == 0)
-						hputs(NO_TAG);
-				} else if (strcmp(a, "BODY") == 0) {
-					while (fgets(body, BUFSIZ,
-					    fbody) != NULL)
-						hputs(body);
-				} else if (strcmp(a, "URL") == 0)
-					hput_url(aname, NULL);
-				else if (strcmp(a, "NB_COMMENTS") == 0) {
-#ifdef ENABLE_COMMENTS
-					switch (nb_comments) {
-					case 0:
-						hputs("no comment");
-						break;
-					case 1:
-						hputs("1 comment");
-						break;
-					default:
-						hputd(nb_comments);
-						hputs(" comments");
-					}
-#endif /* ENABLE_COMMENTS */
-				}
-			}
-		}
-		hputs(a);
-		hputc('\n');
-	}
+	parse_markers(fin, markers_article_content, &d);
 	fclose(fin);
 	++nb_articles;
 }
@@ -547,43 +575,65 @@ render_tag(const char *tag, uint nb_articles)
 	hputs("</span> ");
 }
 
+static void
+markers_tags(const char *m, void *data)
+{
+	if (strcmp(m, "TAGS") == 0)
+		read_tags(render_tag);
+}
+
 void
 render_tags(const char *_)
 {
 	FILE *fin;
-	char buf[BUFSIZ], *a, *b;
 
 	if ((fin = open_template("tags.html")) == NULL)
 		return;
-	while (fgets(buf, BUFSIZ, fin) != NULL) {
-		buf[strcspn(buf, "\n")] = '\0';
-		for (a = buf; (b = strstr(a, TAG)) != NULL; a = b+2) {
-			*b = '\0';
-			hputs(a);
-			a = b+2;
-			if ((b = strstr(a, TAG)) != NULL) {
-				*b = '\0';
-				if (render_generic_markers(a));
-				else if (strcmp(a, "TAGS") == 0)
-					read_tags(render_tag);
-			}
-		}
-		hputs(a);
-		hputc('\n');
-	}
+	parse_markers(fin, markers_tags, NULL);
 	fclose(fin);
 }
 
-void
-render_page(page_cb cb, const char *data)
+static void
+markers_page(const char *m, void *data)
 {
-	FILE *fin;
-	char buf[BUFSIZ], *a, *b;
+	struct data_p *d = data;
 	extern const char *tag;
 	extern long offset;
-#ifdef ENABLE_STATIC
-	time_t mtime;
-#endif /* ENABLE_STATIC */
+
+	if (strcmp(m, "TITLE") == 0) {
+		if (d->cb == render_article && d->name == NULL) {
+			if (tag != NULL) {
+				hputs("- tag:");
+				hputs(tag);
+			}
+		} else if (d->cb == render_article) {
+			char title[BUFSIZ];
+
+			*title = '\0';
+			read_article(d->name, NULL, title, BUFSIZ);
+			if (*title != '\0') {
+				hputs("- ");
+				hputs(title);
+			}
+		} else if (d->cb == render_tags)
+			hputs("- tags");
+	} else if (strcmp(m, "NEXT") == 0) {
+		if (d->cb == render_article && offset > 0)
+			hput_pagelink(tag, offset-1, NAV_NEXT);
+	} else if (strcmp(m, "PREVIOUS") == 0) {
+		if (d->cb == render_article && nb_articles > NB_ARTICLES)
+			hput_pagelink(tag, offset+1, NAV_PREVIOUS);
+	} else if (strcmp(m, "BODY") == 0) {
+		if (d->cb != NULL)
+			d->cb(d->name);
+	}
+}
+
+void
+render_page(page_cb cb, const char *name)
+{
+	FILE *fin;
+	struct data_p d = { cb, name };
 
 	nb_articles = 0;
 	if ((fin = open_template("page.html")) == NULL)
@@ -600,57 +650,21 @@ render_page(page_cb cb, const char *data)
 		fflush(hout);
 #ifdef ENABLE_STATIC
 	}
-	if (from_cmd && cb == render_article && data != NULL
-	    && (mtime = get_mtime(data)) != 0) {
-		strftime(buf, BUFSIZ, MOD_FORMAT, localtime(&mtime));
-		hputs(MOD_BEGIN);
-		hputs(buf);
-		hputs(MOD_END);
-		hputc('\n');
+	if (from_cmd && cb == render_article && name != NULL) {
+		time_t mtime;
+
+		if ((mtime = get_mtime(name)) != 0) {
+			char buf[BUFSIZ];
+
+			strftime(buf, BUFSIZ, MOD_FORMAT, localtime(&mtime));
+			hputs(MOD_BEGIN);
+			hputs(buf);
+			hputs(MOD_END);
+			hputc('\n');
+		}
 	}
 #endif /* ENABLE_STATIC */
-	while (fgets(buf, BUFSIZ, fin) != NULL) {
-		buf[strcspn(buf, "\n")] = '\0';
-		for (a = buf; (b = strstr(a, TAG)) != NULL; a = b+2) {
-			*b = '\0';
-			hputs(a);
-			a = b+2;
-			if ((b = strstr(a, TAG)) != NULL) {
-				*b = '\0';
-				if (render_generic_markers(a));
-				else if (strcmp(a, "TITLE") == 0) {
-					if (cb == render_article
-					    && data == NULL) {
-						if (tag != NULL) {
-							hputs("- tag:");
-							hputs(tag);
-						}
-					} else if (cb == render_article) {
-						char title[BUFSIZ];
-
-						*title = '\0';
-						read_article(data, NULL,
-						    title, BUFSIZ);
-						if (*title != '\0') {
-							hputs("- ");
-							hputs(title);
-						}
-					} else if (cb == render_tags)
-						hputs("- tags");
-				} else if (strcmp(a, "NEXT") == 0
-				    && cb == render_article
-				    && offset > 0)
-					hput_pagelink(tag, offset-1, NAV_NEXT);
-				else if (strcmp(a, "PREVIOUS") == 0
-				    && nb_articles > NB_ARTICLES)
-					hput_pagelink(tag, offset+1, NAV_PREVIOUS);
-				else if (strcmp(a, "BODY") == 0 && cb != NULL)
-					cb(data);
-			}
-		}
-		hputs(a);
-		hputc('\n');
-	}
+	parse_markers(fin, markers_page, &d);
 	fclose(fin);
 }
 
